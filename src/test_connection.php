efcfb7df1337b3fa6426dcd743ae28c876f89ee4
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-24 10:43:01 +0200

    connection test ok

---
+++ src/test_connection.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('connect.php')
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste de connection</title>
+</head>
+<body>
+    <h1>Test connect</h1>
+</body>
+</html>
