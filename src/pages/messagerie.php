cfefb4266bfef98892dfd96d087dc3c60c9c5c49
Malibu <malibu1106@gmail.com>
2024-06-24 13:58:18 +0200

    Pages - messagerie

--- src/pages/messagerie.php
+++ src/pages/messagerie.php
@@ -1,19 +1,17 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    
+    <title>Interface de Messagerie</title>
+
+    <link rel="stylesheet" href="../CSS/messagerie.css">
+   
 </head>
 <body>
 
-<?php require_once ('elements/header.php');?>
 
-<?php require_once ('elements/footer.php');?>
 
-    
+
 </body>
 </html>
-
