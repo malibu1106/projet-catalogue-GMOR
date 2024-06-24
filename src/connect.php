064a5b251c32c84ccdb96299f5fe0abc0d7e3468
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-24 10:18:31 +0200

    connect et disconnect

---
+++ src/connect.php
@@ -0,0 +1,17 @@
+<?php 
+const DBHOST = 'db';
+const DBNAME = 'catalogue_gmor';
+const DBUSER = 'gmor';
+const DBPASS = 'gmor_password';
+
+    //c'est ça qu'il nous manquait ! le ";" veut lui signifier "ça y est c'est fait"
+$dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8";
+
+    //Maintenant, on se connecte avec le PDO
+try {
+$db = new PDO($dsn, DBUSER, DBPASS);
+// echo "connexion réussie";
+} catch(PDOException $error) {
+    echo "échec de la connection: " . $error->getMessage() . "</br>";
+}
+//ci-dessus la fonction nous sort le message d'erreur en cas d'échec
