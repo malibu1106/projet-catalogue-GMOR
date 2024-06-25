3a5e8e88653a18215a6d7846eadda5ff7d1a15f8
Malibu <malibu1106@gmail.com>
2024-06-25 15:51:44 +0200

    search

--- src/elements/header.php
+++ src/elements/header.php
@@ -3,4 +3,11 @@
     <a href="#">
         <img class="logo_index" src="../img/illustration/logo-online-training.png">
     </a>
+    <div class="researchBar">
+        <form class="searchForm" method="GET" action="pages/search.php">
+            <input type="text" class="researchInput" placeholder="Rechercher un article">
+            <button><img class="searchButton" src="../img/illustration/search.png"></button>            
+        </form>
+
+    </div>
 </header>
