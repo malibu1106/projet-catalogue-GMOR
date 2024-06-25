b0f50b3cc21c44146965955d2a432f60e4cc72c2
Malibu <malibu1106@gmail.com>
2024-06-25 16:55:55 +0200

    nav style

--- src/elements/header.php
+++ src/elements/header.php
@@ -5,7 +5,7 @@
     </a>
     <div class="researchBar">
         <form class="searchForm" method="GET" action="pages/search.php">
-            <input name="search" type="text" class="researchInput" placeholder="Rechercher un article">
+            <input name="search" type="text" class="researchInput" placeholder="">
             <button><img class="searchButton" src="../img/illustration/search.png"></button>            
         </form>
 
