a3c3e8e585cefb0bc2181caee6828f5416f087a5
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-06-26 15:02:57 +0200

    construction du foreach pour la barra de recherche

--- src/pages/search.php
+++ src/pages/search.php
@@ -15,7 +15,7 @@ $search_name = $db->prepare("SELECT * FROM products WHERE
     id LIKE :id OR
     ref LIKE :ref OR
     brand LIKE :brand OR
-    `size` LIKE :`size` OR
+    size LIKE :size OR
     color LIKE :color OR
     pattern LIKE :pattern OR
     material LIKE :material OR
@@ -45,10 +45,76 @@ $search_name->bindValue(":content", $search, PDO::PARAM_STR);
 $search_name->execute();
 
 // Obtient les résultats
-$result = $search_name->fetchAll(PDO::FETCH_ASSOC);
+$results = $search_name->fetchAll(PDO::FETCH_ASSOC);
 
 // debug
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+// echo '<pre>';
+// print_r($results);
+// echo '</pre>';
 ?>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/produits.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script type="text/javascript" src="JS/script.js" defer></script>
+    
+    <title>Accueil</title>
+    
+</head>
+<body>
+<?php require_once ('../elements/debug.php');?>
+
+<?php require_once ('../elements/header.php');?>
+
+    <h2>Nom catégorie</h2>
+        <div class="tri">Zone de tri ici</div>
+
+        <section class="affichage_des_produits">
+
+
+    <?php foreach ($results as $result){
+
+        echo '<article class="">
+                <figure class="">
+                    <img class="" src="'. $result['image_1'].'" alt="php name ici">
+                    <figcaption class="">'.$result['brand'].'</figcaption>
+                </figure>
+        </article>';
+    }
+
+    ?>
+
+
+
+        
+
+
+
+
+
+
+        
+
+</section>
+
+<?php require_once ('../elements/footer.php');?>
+
+    
+</body>
+</html>
+
