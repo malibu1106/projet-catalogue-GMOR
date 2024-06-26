676d32ea6f411ba9de97bc4d81eef6be18a5fd27
Malibu <malibu1106@gmail.com>
2024-06-26 09:48:45 +0200

    push debut slide

--- src/JS/slider.js
+++ src/JS/slider.js
@@ -1,1 +1,21 @@
-slider.js
+// scripts.js
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(index) {
+    const slides = document.getElementsByClassName('slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+}
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
