8742760fbe66de21ca86f3ad4e585401d02396d8
Malibu <malibu1106@gmail.com>
2024-06-27 09:52:39 +0200

    suite messagerie + bdd up pour tests

--- src/pages/messagerie.php
+++ src/pages/messagerie.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messagerie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/messagerie.css">
 </head>
 <body>
+
+<?php require_once ('../elements/debug.php');?>
+<?php require_once ('../elements/header.php');?>
     <!-- VERSION UTILISATEUR -->
     <section class="messagerie">
         <div class="messagerie_top">
@@ -21,6 +26,10 @@
             </div>
         </div>
         <div class="messagerie_main">
+            <div class="message_received">reçu
+            </div>
+            <div class="message_sent">envoyé
+            </div>
         </div>
     </section>
     <!-- FIN VERSION UTILISATEUR -->
