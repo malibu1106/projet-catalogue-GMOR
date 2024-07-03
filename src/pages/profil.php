94cce5d0f7b07b0db6130c351de41e2b71673354
yrautcnasdemo <yrautcnas@msn.com>
2024-07-03 13:40:05 +0200

    Recup Cart.css

--- src/pages/profil.php
+++ src/pages/profil.php
@@ -123,13 +123,34 @@ if ($_POST && isset($_POST["ref"]) && isset($_POST["brand"]) && isset($_POST["si
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../CSS/backoffice-style.css">
     <title>Profile</title>
 </head>
-<body class="backoffice-profil">
-    <?php require_once('../elements/header.php'); ?>
+<?php require_once('../elements/header.php'); ?>
+<body>
 
+    <main class="backoffice-profil">
+        <div class="profil-card">
+            <div class="profil-circle">
+                <div class="imgBx">
+                    <img src="/img/temporaire/No-trep.png" alt="">
+                </div>
+            </div>
+            <div class="profil-content">
+                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <h3 class="profil-name">Blue Marry</h3>
+                <div class="mailIcon">
+                    <h4 class="profil-roles">Admin Comptable</h4>
+                    <a href="#">
+                    <i class="fa fa-envelope-open" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
     <script type="text/javascript" src="../JS/script.js" defer></script>
     <?php require_once('../elements/footer.php'); ?>
-</body>
 </html>
