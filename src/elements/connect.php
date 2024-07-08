a4deebcc62c3aeed639b02934e73d6bc0ba7eabc
morganencr <morgane.encrenaz@gmail.com>
2024-07-08 14:20:48 +0200

    suggestions

--- src/elements/connect.php
+++ src/elements/connect.php
@@ -11,7 +11,9 @@ $dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8";
 try {
 $db = new PDO($dsn, DBUSER, DBPASS);
 // echo "connexion réussie";
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Activer le mode d'erreur PDO
 } catch(PDOException $error) {
-    // echo "échec de la connection: " . $error->getMessage() . "</br>";
+    echo "échec de la connection: " . $error->getMessage() . "</br>";
+    exit(); // Arrêter le script en cas d'erreur de connexion
 }
 //ci-dessus la fonction nous sort le message d'erreur en cas d'échec
