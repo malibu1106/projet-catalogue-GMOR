6b1b1e10377db4505a440f386503809c7148106f
osias-espindola <167890686+osias-espindola@users.noreply.github.com>
2024-07-10 10:55:18 +0200

    Commentaires et messages d’erreur sur frances

--- src/pages/archive_order.php
+++ src/pages/archive_order.php
@@ -8,7 +8,7 @@ if (isset($_GET['id'])) {
     try {
         $db->beginTransaction();
 
-        // Verificar se a comanda já está arquivada
+        // Vérifier si la commande est déjà archivée
         $sql_check = "SELECT COUNT(*) FROM archive WHERE order_id = :order_id";
         $stmt_check = $db->prepare($sql_check);
         $stmt_check->bindParam(':order_id', $order_id, PDO::PARAM_INT);
@@ -16,27 +16,27 @@ if (isset($_GET['id'])) {
         $is_archived = $stmt_check->fetchColumn();
 
         if ($is_archived > 0) {
-            $_SESSION['info'] = "Esta comanda já está arquivada.";
+            $_SESSION['info'] = "Cette commandante est déjà sur l'archive ";
             $db->commit();
             header("Location: ../pages/commandes.php");
             exit();
         }
 
-        // Recuperar dados da comanda
+        // Récupérer les données de la commande
         $sql_order = "SELECT * FROM orders WHERE id = :order_id";
         $stmt_order = $db->prepare($sql_order);
         $stmt_order->bindParam(':order_id', $order_id, PDO::PARAM_INT);
         $stmt_order->execute();
         $order = $stmt_order->fetch(PDO::FETCH_ASSOC);
 
-        // Recuperar itens da comanda
+        // Récupérer les items de la commande
         $sql_items = "SELECT * FROM order_items WHERE order_id = :order_id";
         $stmt_items = $db->prepare($sql_items);
         $stmt_items->bindParam(':order_id', $order_id, PDO::PARAM_INT);
         $stmt_items->execute();
         $items = $stmt_items->fetchAll(PDO::FETCH_ASSOC);
 
-        // Inserir dados na tabela archive
+        // Insérer des données dans la table archive
         foreach ($items as $item) {
             $sql_archive = "INSERT INTO archive (cart_id, order_id, order_items_id, product_id, quantity_id, price_id) 
                             VALUES (:cart_id, :order_id, :order_items_id, :product_id, :quantity_id, :price_id)";
@@ -51,19 +51,20 @@ if (isset($_GET['id'])) {
             ]);
         }
 
-       
 
+        // Message d’erreur ou succès lors de l’archivage des données 
         $db->commit();
-        $_SESSION['success'] = "Comanda arquivada com sucesso.";
+        $_SESSION['success'] = "Commande archivée avec succès.";
     } catch (Exception $e) {
         $db->rollBack();
-        $_SESSION['error'] = "Erro ao arquivar comanda: " . $e->getMessage();
+        $_SESSION['error'] = "Erreur de classement commande: " . $e->getMessage();
     }
 
     header("Location: ../pages/commandes.php");
     exit();
+    // Message d’erreur manquant de l’id  
 } else {
-    $_SESSION['error'] = "ID da comanda não fornecido.";
+    $_SESSION['error'] = "ID de commande non fourni.";
     header("Location: ../pages/commandes.php");
     exit();
 }
